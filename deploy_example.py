c985f6f86fc3bf87f3046b11db503735cba7332c
Jeremy Banks <jeremy@jeremybanks.ca>
2012-06-28 15:59:28 -0400

    adding Python script to upload demo to s3

---
+++ deploy_example.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import os
+import os.path
+import webbrowser
+from itertools import islice
+from boto.s3.connection import S3Connection
+
+# set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY in env.
+
+s3 = S3Connection()
+bucket = S3Connection().create_bucket("slide-drive-demo")
+
+
+print "Uploading modified examples/demo.html"
+
+demo_source = open("examples/demo.html", "rb").read().replace("http://localhost:8888/", "/")
+
+demo_key = bucket.new_key("examples/demo.html")
+demo_key.metadata.update({ "Content-Type": "text/html" })
+demo_key.set_contents_from_string( demo_source )
+demo_key.make_public()
+
+bulk_paths = [
+  "js",
+  "css",
+  "examples/demo-audio.mp3",
+  "external/deckjs",
+  "external/jquery",
+  "external/mediaelement",
+  "external/modernizr",
+  "external/butter/src/butter.js",
+  "external/butter/external/popcorn-js/popcorn.js",
+  "external/butter/external/require"
+]
+
+for path in bulk_paths:
+  if os.path.isfile( path ):
+    print "Uploading", path
+    key = bucket.new_key( path )
+    key.set_contents_from_file( open(path, "rb") )
+    key.make_public()
+  
+  elif os.path.isdir( path ):
+    for path_prefix, _, filenames in os.walk( path ):
+      for filename in filenames:
+        full_path = path_prefix + "/" + filename
+        print "Uploading", full_path
+        
+        key = bucket.new_key( full_path )
+        key.set_contents_from_file( open(full_path, "rb") )
+        key.make_public()
+  
+  else:
+    print "Skipping non-file non-directory", path
+
+
+webbrowser.open("http://slide-drive-demo.s3.amazonaws.com/examples/demo.html")
